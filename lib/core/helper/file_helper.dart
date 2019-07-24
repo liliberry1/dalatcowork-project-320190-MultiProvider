b9a206c37e87e294d680e97bbba41ab5756ae8cb
liliberry1 <tinhpt1@dalatcowork.com>
2019-07-24 11:14:41 +0700

    create helper and file helper

---
+++ lib/core/helper/file_helper.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+
+
+
+class FileHelper{
+
+  Future<String> get localPath async{
+    var dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  Future<File> get localFile async{
+    final path = localPath;
+    return File('$path/accTke22145.txt');
+  }
+
+  Future<File> writeData(String content) async{
+    final file = await localFile;
+    return file.writeAsString(content);
+  }
+
+  Future<int> readAccessToken() async {
+    try {
+      final file = await localFile;
+      // Read the file
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+
+}
---
+++ lib/core/helper/helper.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+
+
+class Helper{
+
+  saveAccessToken(String accessToken){
+
+  }
+
+}
